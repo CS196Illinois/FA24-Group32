8afa2987d08d2af587186c6d122b14b8b22acb39
Agastya Govind <79813984+ARGH23@users.noreply.github.com>
2024-10-14 17:35:07 -0500

    Updating app.py
    Change to include correct html path

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     #Change this later to include html details
-    return render_template('index.html')
+    return render_template('Project/public/index.html')
 
 
 if __name__ == '__main__':
