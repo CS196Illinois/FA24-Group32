d5f0580a14dc14ea3749cbfd4a1668934d80e961
Interspectral6 <tianle06@163.com>
2024-10-18 16:35:23 -0500

    Backend small adjustments

--- backend.py
+++ backend.py
@@ -1,12 +1,13 @@
 import requests
 import config
 
+#will return the time in minutes
 def get_walk_time(origin, destination, api_key):
     url = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={origin}&destinations={destination}&mode=walking&key={api_key}"
     response = requests.get(url).json()
     if response["status"] == "OK":
         #print(response) #for debug purposes
-        return response["rows"][0]["elements"][0]["duration"]["value"]
+        return response["rows"][0]["elements"][0]["duration"]["value"]/60
     else:
         return "Error"
 
@@ -23,7 +24,7 @@ def calculate_centroid(locations):
 diningHalls = ("Ikenberry Dining Center", "Illinois Street Dining Center", "Lincoln Avenue Dining Hall (LAR Dining Hall)", "Pennsylvania Avenue (PAR) Dining Hall")
 
 #build the locations array based on fixed list of destinations and member's locations
-def build_locations(destinations, memberLocations, api_key):
+def build_locations(memberLocations, destinations, api_key):
     #destinations is the list of destinations, we use diningHalls for the MVP. memberLocations is the list of group member's locations
     
     if not destinations:
@@ -35,6 +36,7 @@ def build_locations(destinations, memberLocations, api_key):
     locations = []
 
     for destination in destinations:
+        #print(destination)
         travelTime = []
         for memberLocation in memberLocations:
             travelTime.append(get_walk_time(memberLocation, destination, api_key))
@@ -43,7 +45,7 @@ def build_locations(destinations, memberLocations, api_key):
     return locations
 
 #returns the best location/s for the group
-def find_optimal_locations(locations, maximumTravelDistance):
+def find_optimal_locations(memberLocations, destinations, api_key, maximumTravelDistance):
     '''
     locations will be an array of dictionaries containing the possible locations the group can meet at 
     each dictionary in locations will have the name stored as locations[i]['name']
@@ -52,6 +54,8 @@ def find_optimal_locations(locations, maximumTravelDistance):
     (e.g. you might prefer to go to something that is 10 minutes away from everyone rather than something that is 30 minutes away from one person and 1 minute away from 3 other people
     Can add more of these requirements later
     '''
+    locations = build_locations(memberLocations, destinations, api_key)
+
     fulfillsAllReqs = {}
     otherLocations = {}
 
@@ -61,25 +65,25 @@ def find_optimal_locations(locations, maximumTravelDistance):
     for location in locations:
         #note, we can develop a more sophisticated way to score a location than just the sum of times
         sumTimes = sum(location['travelTimes'])
-        times = location['travelTimes']
-        times.sort()
-        if (times[-1] > maximumTravelDistance):
+        if (max(location['travelTimes']) > maximumTravelDistance):
             otherLocations[location['name']] = sumTimes
         else:
-            fulfillsAllReqs[location['name']] = sumTimes
+            fulfillsAllReqs[location['name']] = sumTimes  
 
     if fulfillsAllReqs != {}:
         #efficient way to find minimum key in a dictionary: https://www.geeksforgeeks.org/python-minimum-value-keys-in-dictionary/
         res =  [key for key in fulfillsAllReqs if
         all(fulfillsAllReqs[temp] >= fulfillsAllReqs[key]
-        for temp in fulfillsAllReqs)]
+        for temp in fulfillsAllReqs)][0]
     else:
         res =  [key for key in otherLocations if
         all(otherLocations[temp] >= otherLocations[key]
-        for temp in otherLocations)]
+        for temp in otherLocations)][0]
+        print("Over max time")
 
     return res
 
 if __name__ == "__main__":
-    #print(get_walk_time("Ikenberry Dining Center", "Grainger Library", config.api_key))
-    print(find_optimal_locations(build_locations(diningHalls, places, config.api_key), 1000))
+    places = ("grainger library", "illini union")
+    #print(get_walk_time("Pennsylvania Avenue (PAR) Dining Hall", "Grainger Library", config.api_key))
+    print(find_optimal_locations(places, diningHalls, config.api_key, 10))
