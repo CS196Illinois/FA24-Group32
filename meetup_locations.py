59d496ebaeabd4134e8b7a34ed1e1ab5bf7e08ad
nrchen2 <nrchen2@illinois.edu>
2024-11-18 18:07:34 -0600

    Update meetup_locations.py

--- meetup_locations.py
+++ meetup_locations.py
@@ -12,7 +12,7 @@ MEETUP_SPOTS = [
 ]
 
 # Initialize Google Maps client
-API_KEY = "AIzaSyBnDgIgz2FbuGMJXeylzsLf5SjujtSP31o"
+API_KEY = "api_key"
 gmaps = googlemaps.Client(key=API_KEY)
 
 def get_user_locations():
